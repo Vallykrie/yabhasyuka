9f67d82a07c5f382c6b050af545d1317b7c45f10
Nathan Sudiara <nathansudiara@gmail.com>
2025-10-19 17:34:30 +0700

    fix: metadata image and desciption on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import bg from "@/public/7582.jpg"
 import "./globals.css"
 
 const inter = Inter({
@@ -11,20 +12,20 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Yabhasyuka Aesthetics",
-  description: "",
+  description: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions. | Yabhasyuka",
   generator: "v0.app",
   alternates: {
     canonical: "https://yabhasyuka.example/",
   },
   openGraph: {
-    siteName: "Katachi",
+    siteName: "Yabhasyuka",
     title: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions. | Yabhasyuka",
-    description: "",
+    description: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions. | Yabhasyuka",
     type: "website",
     url: "https://yabhasyuka.vercel.app/",
     images: [
       {
-        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
+        url:"https://img.freepik.com/free-photo/young-woman-mask-face-relaxing-spa-salon_176420-7582.jpg",
         alt: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions.",
         width: 1200,
         height: 630,
@@ -35,14 +36,14 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions. | Yabhasyuka",
-    description: "Architected in Belgium, built to last—timeless pieces.",
+    description: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions.",
     images: [
       {
         url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/opengraph-katachi.jpg-7vz2r3hxZA6woukGOmH115Fg7Piyjs.jpeg",
         alt: "Aesthetics Clinic specializing in anti-aging treatments and skincare solutions.",
       },
     ],
-    site: "@katachi",
+    site: "@yabhasyuka",
   },
 }
 
